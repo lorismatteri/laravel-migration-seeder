0d1a5dc02363e3b25e119e22244903826645c02f
Loris Matteri <loris.matteri93@gmail.com>
2021-02-22 17:38:42 +0100

    Classi con faker

--- database/seeds/ClasseSeeder.php
+++ database/seeds/ClasseSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Classe;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ClasseSeeder extends Seeder
 {
@@ -10,16 +11,23 @@ class ClasseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         //
-        $classes_list = config('classes');
+        /* $classes_list = config('classes');
 
         foreach ($classes_list as $class) {
             $newClass = new Classe();
             $newClass->number = $class['number'];
             $newClass->students_number = $class['students_number'];
             $newClass->course = $class['course'];
+            $newClass->save();
+        } */
+        for($i=0; $i<100; $i++) {
+            $newClass = new Classe();
+            $newClass->number = $faker->numberBetween(1, 100);
+            $newClass->students_number = $faker->numberBetween(1, 250);
+            $newClass->course = $faker->word();
             $newClass->save();
         }
     }
